0fbbd41386dda189937ca88f2b5245bd50a9172e
IstarVin <ajdeleverio20@gmail.com>
2025-07-25 08:07:14 +0800

    added ai chatbot

---
+++ src/lib/server/services/chatbot/index.ts
@@ -0,0 +1,35 @@
+export async function sendMessage(msg: string, sessionId: string) {
+	if (!process.env.N8N_CHATBOT_URL) {
+		console.error('N8N_CHATBOT_URL is not set');
+		return;
+	}
+
+	try {
+		const res = await fetch(process.env.N8N_CHATBOT_URL, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: 'Bearer ' + process.env.N8N_CHATBOT_KEY
+			},
+			body: JSON.stringify({
+				chatInput: msg,
+				sessionId,
+				action: 'sendMessage'
+			})
+		});
+
+		if (!res.ok) {
+			return `Chatbot API request failed with status ${res.status}`
+		}
+
+		const [json] = (await res.json()) as { output: string }[];
+
+		if (!json?.output) {
+			return 'Invalid response format from chatbot API'
+		}
+
+		return json.output;
+	} catch (error) {
+		return 'Failed to send message to chatbot: ' + error
+	}
+}
